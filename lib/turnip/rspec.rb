9d0fde350b2bdc97af82523c8a3d39e1a83ca66b
nakaji <nakaji@h28is154.naist.jp>
2018-12-17 16:57:54 +0900

    環境変数追加

--- lib/turnip/rspec.rb
+++ lib/turnip/rspec.rb
@@ -1,6 +1,6 @@
 require "turnip"
 require "rspec"
-
+require 'pry'
 module Turnip
   module RSpec
 
@@ -14,8 +14,11 @@ module Turnip
         if a.first.end_with?('.feature')
           require_if_exists 'turnip_helper'
           require_if_exists 'spec_helper'
-
-          Turnip::RSpec.run(a.first)
+          if ENV["notest"]
+            Turnip::RSpec.notest_run(a.first)
+          else
+            Turnip::RSpec.run(a.first)
+          end
         else
           super
         end
@@ -70,6 +73,35 @@ module Turnip
       end
 
       def run(feature_file)
+        #ここでfeatureファイルをfeatureファイルとして実行している
+        Turnip::Builder.build(feature_file).features.each do |feature|
+          ::RSpec.describe feature.name, feature.metadata_hash do
+            before do
+              example = Turnip::RSpec.fetch_current_example(self)
+              # This is kind of a hack, but it will make RSpec throw way nicer exceptions
+              example.metadata[:file_path] = feature_file
+              #ここで一つのfeatureは見れる　backgroundが何かはわからん…
+              feature.backgrounds.map(&:steps).flatten.each do |step|
+                run_step(feature_file, step)
+              end
+            end
+            feature.scenarios.each do |scenario|
+              instance_eval <<-EOS, feature_file, scenario.line
+                describe scenario.name, scenario.metadata_hash do
+                  it(scenario.steps.map(&:description).join(' -> ')) do
+                    scenario.steps.each do |step|
+                      run_step(feature_file, step)
+                    end
+                  end
+                end
+              EOS
+            end
+          end
+        end
+      end
+
+      def notest_run(feature_file)
+        puts "テスト生成用のコマンドでテストは実行されません"
         Turnip::Builder.build(feature_file).features.each do |feature|
           ::RSpec.describe feature.name, feature.metadata_hash do
             before do
@@ -84,30 +116,17 @@ module Turnip
             #TODO ファイルパス修正
             feature.scenarios.each do |scenario|
               instance_eval <<-EOS, feature_file, scenario.line
-                describe scenario.name, scenario.metadata_hash do
-                  before :all do
                     File.open(Rails.root.to_s + "/sizen.txt","a") do |f|
                       f.puts "#機能: " + scenario.id
                       f.puts "describe '#{scenario.name}' do"
                     end
-                  end
-                  it(scenario.steps.map(&:description).join(' -> ')) do
                     File.open(Rails.root.to_s + "/sizen.txt","a") do |f|
                       f.puts "  it '#{scenario.steps.map(&:description).join(' -> ').gsub(/'/,"%")}' do"
-                    end
-                    scenario.steps.each do |step|
-                      run_step(feature_file, step)
-                    end
-                    File.open(Rails.root.to_s + "/sizen.txt","a") do |f|
                       f.puts "  end"
                     end
-                  end
-                  after :all do
                     File.open(Rails.root.to_s + "/sizen.txt","a") do |f|
                       f.puts "end"
                     end
-                  end
-                end
               EOS
             end
           end
